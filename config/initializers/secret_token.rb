64a6b8c6f84e0c7b8a2023b3b81abf7e3a89911a
Alexander Keramidas <alexkeramidas@gmail.com>
2013-09-28 18:09:30 +0300

    New framework installation with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BelvedereGit::Application.config.secret_key_base = '361d4f45d28567d09f53fa9eb19ca872935e1fa2395cb828c9f26ed2dd6cfc242a79e8fb40009393283e615f55d95c26e0753a001323bf9d88d75f970e6cce7d'
