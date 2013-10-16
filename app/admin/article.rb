800556120dfd533c9495662c88f30ada47a88e0e
alexkeramidas <alexkeramidas@gmail.com>
2013-10-16 19:35:54 +0300

    fixed backend appearance for rooms and news

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,17 +1,63 @@
-ActiveAdmin.register Article do
+ActiveAdmin.register Article, :as => 'News' do
+
+    config.per_page = 10
+
+    index do
+        column :title
+        column :description
+        column :visible
+        column :created_at
+        column :updated_at
+        default_actions
+    end
+
     form do |f|
         f.inputs do
-            f.input :title
-            f.input :description, :input_html => { :class => "tinymce-jquery"}
-            f.input :visible
-            f.input :article_type
+            f.input :title, :input_html => {:name => "article[title]"}
+            f.input :description, :input_html => { :class => "tinymce-jquery", :name => "article[description]"}
+            f.input :visible, :input_html => {:name => "article[visible]"}
         end
     f.buttons
     end
 
     controller do
         def permitted_params
-            params.permit article: [:title, :description, :visible, :article_type]
+            params.permit article: [:title, :description, :visible, :article_type => 1]
+        end
+        def scoped_collection
+            Article.where(:article_type => 1)
+        end
+    end
+end
+
+ActiveAdmin.register Article, :as => 'Room' do
+
+    config.per_page = 10
+
+    index do
+        column :title
+        column :description
+        column :visible
+        column :created_at
+        column :updated_at
+        default_actions
+    end
+
+    form do |f|
+        f.inputs do
+            f.input :title, :input_html => {:name => "article[title]"}
+            f.input :description, :input_html => { :class => "tinymce-jquery", :name => "article[description]"}
+            f.input :visible, :input_html => {:name => "article[visible]"}
+        end
+    f.buttons
+    end
+
+    controller do
+        def permitted_params
+            params.permit article: [:title, :description, :visible, :article_type => 2]
+        end
+        def scoped_collection
+            Article.where(:article_type => 2)
         end
     end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
-    
+
     # GET /articles
     # GET /articles.json
     def index
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
     # GET /articles/1
     # GET /articles/1.json
     def show
-        
+
     end
 
     #################
@@ -70,7 +70,7 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-        params.require(:article).permit(:title, :description, :visible, :article_type)
+        params.require(:article).permit(:title, :description, :visible)
     end
 
     #################
