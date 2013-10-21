b592e1d949014a346db4fb3e152e7b759e015e67
Irene Spanou <e.spanou.82@gmail.com>
2013-10-21 16:31:54 +0300

    Changed photos controller inheritance

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,4 @@
-class PhotosController < InheritedResource::Base
+class PhotosController < ApplicationController
     def photo_params
         params.require(:image).permit(:article, :description, :image_file_name, :image_content_type, :image_file_size, :image_updated_at)
     end
