b22b344e68a72768480fb9c11b9cc4a29509143e
Irene Spanou <e.spanou.82@gmail.com>
2013-10-22 18:54:52 +0300

    Added the correct image paths for uploaded photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,9 @@
 class Photo < ActiveRecord::Base
     belongs_to :suite
     
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    has_attached_file :image,
+                      :url => "/photos/:style/:filename",
+                      :path => ":rails_root/public/photos/:style/:filename",
+                      :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                      :default_url => "/images/:style/missing.png"
 end
