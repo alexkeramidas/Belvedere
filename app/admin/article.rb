afb551dd89650c5872bc2bc09b34c4822553085d
alexkeramidas <alexkeramidas@gmail.com>
2013-10-24 16:17:35 +0300

    Functional News and Rooms in Active Admin without Images

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -13,24 +13,24 @@ ActiveAdmin.register Article, :as => 'News' do
 
     form do |f|
         f.inputs do
-            f.input :title, :input_html => {:name => "article[title]"}
-            f.input :description, :input_html => { :class => "tinymce-jquery", :name => "article[description]"}
-            f.input :visible, :input_html => {:name => "article[visible]"}
+            f.input :title
+            f.input :description, :input_html => { :class => "tinymce-jquery" }
+            f.input :visible
         end
     f.buttons
     end
 
     controller do
         def permitted_params
-            params.permit article: [:title, :description, :visible, :article_type => 1]
+            params.permit news: [:title, :description, :visible, :article_type => 1]
         end
         def scoped_collection
-            Article.where(:article_type => 1)
+            Article.valid
         end
     end
 end
 
-ActiveAdmin.register Article, :as => 'Room' do
+ActiveAdmin.register Suite, :as => 'Rooms' do
 
     config.per_page = 10
 
@@ -45,19 +45,19 @@ ActiveAdmin.register Article, :as => 'Room' do
 
     form do |f|
         f.inputs do
-            f.input :title, :input_html => {:name => "article[title]"}
-            f.input :description, :input_html => { :class => "tinymce-jquery", :name => "article[description]"}
-            f.input :visible, :input_html => {:name => "article[visible]"}
+            f.input :title
+            f.input :description, :input_html => { :class => "tinymce-jquery" }
+            f.input :visible
         end
     f.buttons
     end
 
     controller do
         def permitted_params
-            params.permit article: [:title, :description, :visible, :article_type => 2]
+            params.permit rooms: [:title, :description, :visible, :article_type => 2]
         end
         def scoped_collection
-            Article.where(:article_type => 2)
+            Suite.valid
         end
     end
 end
--- app/controllers/suites_controller.rb
+++ app/controllers/suites_controller.rb
@@ -1,20 +1,5 @@
 class SuitesController < ApplicationController
-    before_action :set_room, only: :show
-    
-    # GET /articles
-    # GET /articles.json
     def index
         @rooms = Suite.valid.visible
-    end
-
-    # GET /articles/1
-    # GET /articles/1.json
-    def show
-        
-    end
-
-    # Use callbacks to share common setup or constraints between actions.
-    def set_room
-        @room = Suite.valid.visible.find(params[:id])
     end
 end
