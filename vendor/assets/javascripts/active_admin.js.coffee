c48d18ba9e0a122d7fe2eaa835df1abb26f9b52c
Irene Spanou <e.spanou.82@gmail.com>
2013-11-07 16:44:52 +0200

    Rich Editor toolbar configuration

--- vendor/assets/javascripts/active_admin.js.coffee
+++ vendor/assets/javascripts/active_admin.js.coffee
@@ -1,2 +1,3 @@
 #= require active_admin/base
 #= require rich
+#= require rich_config
---
+++ vendor/assets/javascripts/rich_config.js
@@ -0,0 +1,8 @@
+CKEDITOR.config.clipboard_defaultContentType = 'text';
+CKEDITOR.config.forcePasteAsPlainText = true;
+
+CKEDITOR.config.font_names =
+    'Della Respira/Della Respira, DellaRespira-Regular, serif;' +
+    'Century Gothic/Century Gothic, verdana, arial, helvetica, sans-serif';
+
+CKEDITOR.config.fontSize_sizes = '10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;';
