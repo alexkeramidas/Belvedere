f3390519d8c69b04d24aa246e01a758e8b60ba67
Alexander Keramidas <dev.alexkeramidas@gmail.com>
2013-11-11 11:02:41 +0200

    Fixes #39

--- app/admin/suite.rb
+++ app/admin/suite.rb
@@ -41,21 +41,19 @@ ActiveAdmin.register Suite, :as => 'Rooms' do
             f.input :description
             f.input :visible
         end
-        f.inputs "Photos" do
-            f.has_many :photos do |p|
-                unless p.object.new_record?
-                    p.input :_destroy, :as=>:boolean, :label => "Delete Image?"
-                end
-                p.input :description
-                p.input :image, :as => :file, :hint => p.object.new_record? ? "" : p.template.image_tag(p.object.decorate.photo_path(:thumb))
+        f.has_many :photos do |p|
+            unless p.object.new_record?
+                p.input :_destroy, :as=>:boolean, :label => "Delete Image?", :required => false
             end
+            p.input :description, :label => "Image Title"
+            p.input :image, :as => :file, :hint => p.object.new_record? ? "" : p.template.image_tag(p.object.decorate.photo_path(:thumb)), :label => p.object.image_file_name
         end
         f.actions
     end
 
     controller do
         def permitted_params
-            params.permit rooms: [:title, :description, :visible, :article_type => 2,  photos_attributes: [:id, :article_id, :description, :image, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :_destroy]]
+            params.permit rooms: [:title, :description, :visible, :article_type => 2,  photos_attributes: [:id, :article_id, :description, :image, :_destroy]]
         end
 
         def scoped_collection
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,2 @@
 class PhotosController < InheritedResources::Base
-    def photo_params
-        params.require(:image).permit(:suite, :description, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :_destroy)
-    end
 end
