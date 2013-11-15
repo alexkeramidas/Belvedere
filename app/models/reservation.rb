e6aac405307e0ed7525041bf302e7f1f029b4fbe
Alexander Keramidas <dev.alexkeramidas@gmail.com>
2013-11-15 18:25:11 +0200

    Added custom validations and some css to beautify

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ActiveRecord::Base
-    validates_format_of :email, :with => /\A([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})\Z/i, :message => 'Email address is not correct (e.g. name@domain.com)'
+    validates :email, :with => :validate_email
     validates_format_of :adults, :with => /\A(0*(?:[1-2][0-9]?|2))\Z/i, :message => 'Maximum number of adults allowed is 19'
     validates_format_of :youngsters, :with => /\A(0*(?:[0-2][0-9]?|2))?\Z/i, :message => 'Maximum number of children allowed is 19'
     validates :arrival, :date => {:after => Proc.new { Time.now + 1.day}, :before => Proc.new { Time.now + 1.day + 6.month } , :message => 'Arrival date must be after current date'}
@@ -8,4 +8,21 @@ class Reservation < ActiveRecord::Base
     def days
         days_to_stay = (departure - arrival).to_i
     end
+
+    validates_each :phone, :mobile do |model, attr, value|
+        left_par = value.count(")")
+        right_par = value.count("(")
+        left_bra = value.count("]")
+        right_bra = value.count("[")
+        if !value.match(/\A^(?!\(\+\))[\(\[]?\+?[\)\]]?\s?(?<num>([\(\[]?\d{1,10}[\)\]]?([\s\-\.\/]{1,3}\g<num>|[\(\[]?\d{1,10}[\)\]]?\d{,10}))?)$\Z/i) || left_par!=right_par || left_bra!=right_bra
+              model.errors.add(attr, "Invalid phone number format!!!")
+        end
+     end
+
+     def validate_email
+        if !self.email.match(/\A([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})\Z/i)
+               self.errors.add(:email, "Email address is not correct (e.g. name@domain.com)!!!")
+        end
+     end
+
 end
