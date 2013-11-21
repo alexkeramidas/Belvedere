2747ba2fd072411250ce600cf6484763ac286002
Alexander Keramidas <dev.alexkeramidas@gmail.com>
2013-11-21 12:44:34 +0200

    Fixes #49

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,10 +1,12 @@
 class Reservation < ActiveRecord::Base
+
+    before_validation :children_to_zero
+
     validates :email, :with => :validate_email
-    
+
     validates_format_of :name, :with => /\A#{ApplicationHelper.form_field_attr('name')[:regex]}\Z/i, :message => "#{ApplicationHelper.form_field_attr('name')[:generic]}"
     validates_format_of :adults, :with => /\A#{ApplicationHelper.form_field_attr('adults')[:regex]}\Z/i, :message => "#{ApplicationHelper.form_field_attr('adults')[:generic]}"
     validates_format_of :youngsters, :with => /\A#{ApplicationHelper.form_field_attr('children')[:regex]}\Z/i, :message => "#{ApplicationHelper.form_field_attr('children')[:generic]}"
-    
     validates :arrival, :date => {:after => Proc.new { 1.day.ago }, :before => Proc.new { 6.months.from_now } , :message => "#{ApplicationHelper.form_field_attr('arrival')[:generic]}"}
     validates :departure, :date => {:after => :arrival , :before => Proc.new { 1.day.since(6.months.from_now.to_date) }, :message => "#{ApplicationHelper.form_field_attr('departure')[:generic]}"}
 
@@ -14,7 +16,7 @@ class Reservation < ActiveRecord::Base
         right_par = value.count("(")
         left_bra = value.count("]")
         right_bra = value.count("[")
-        
+
         if !value.match(/\A^(?!\(\+\))[\(\[]?\+?[\)\]]?\s?(?<num>([\(\[]?\d{1,10}[\)\]]?([\s\-\.\/]{1,3}\g<num>|[\(\[]?\d{1,10}[\)\]]?\d{,10}))?)$\Z/i) || left_par != right_par || left_bra != right_bra
             model.errors.add(attr, "#{ApplicationHelper.form_field_attr(attr.to_s)[:generic]}")
         end
@@ -24,6 +26,13 @@ class Reservation < ActiveRecord::Base
     def validate_email
         if !self.email.match(/\A#{ApplicationHelper.form_field_attr('email')[:regex]}\Z/i)
             self.errors.add(:email, "#{ApplicationHelper.form_field_attr('email')[:generic]}")
+        end
+    end
+
+    #Change children value to zero if set to nil to be used before validation
+    def children_to_zero
+        if self.youngsters == nil
+            self.youngsters = 0
         end
     end
 end
