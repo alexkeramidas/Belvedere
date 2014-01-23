221661c58318989f4e56e1bc5636c42d7fb0b039
Irene Spanou <e.spanou.82@gmail.com>
2014-01-23 15:14:49 +0200

    Configured routes to support locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,18 @@ class ApplicationController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
-
-    before_filter :assign_variables
-
-    before_filter :set_locale
+    
+    before_filter :set_locale, :assign_variables
 
     def default_url_options(options = {})
-        {locale: I18n.locale}
+        { locale: I18n.locale }
     end
 
     private
 
     #Set locale as query parameter to be used in before filter
     def set_locale
-        I18n.locale = params[:locale] if params[:locale].present?
+        I18n.locale = params[:locale] || I18n.default_locale
     end
 
     def assign_variables
