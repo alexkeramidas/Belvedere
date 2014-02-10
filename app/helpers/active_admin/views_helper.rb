431b97fe2d699c3802232e71cb0ca9672b2c823e
Irene Spanou <e.spanou.82@gmail.com>
2014-02-10 12:20:24 +0200

    Minor change for language names used in admin

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -1,7 +1,7 @@
 module ActiveAdmin::ViewsHelper
     def language_list
         I18n.available_locales.map { |loc|
-            loc = [I18n.t("meta.language_name", locale: loc.to_s), loc.to_s]
+            loc = [I18n.t("meta.language_admin", locale: loc.to_s), loc.to_s]
         }
     end
 end
